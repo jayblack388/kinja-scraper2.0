3d2d7f2c15cb6263b78c181c378682df28a29ded
jayblack388 <jblackwell072588@gmail.com>
2019-05-27 22:54:43 -0400

    basic routes

---
+++ routes/api.js
@@ -0,0 +1,30 @@
+const scrape = require('../scripts/scrape');
+
+module.exports = (app, db) => {
+  app.get('/scrape/:id', function(req, res) {
+    const choice = req.params.id;
+    scrape(choice, res);
+  });
+
+  app.get('/headlines', function(req, res) {
+    db.Headline.find({})
+      .then(function(dbArticles) {
+        res.json(dbArticles);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.get('/headlines/:id', function(req, res) {
+    db.Headline.find({
+      _id: req.params.id
+    })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+};
