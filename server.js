405ba6013ef13cb4ff39ea0eb4533d00b1895ab8
John Blackwell <john.blackwell@rtslabs.com>
2019-11-15 07:53:52 -0800

    updating server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const cors = require("cors");
+const cron = require("node-cron");
+const routes = require("./routes");
+const scrape = require("./scripts/scrape");
 /* eslint-disable-next-line */
 const log = console.log;
 
@@ -14,20 +16,25 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(logger('dev'));
+app.use(logger("dev"));
 
-app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
 app.use(routes);
 
 mongoose.Promise = global.Promise;
-const databaseUri = 'mongodb://localhost:27017/kinja-scraper';
+const databaseUri = "mongodb://localhost:27017/kinja-scraper";
 
 mongoose.connect(process.env.MONGODB_URI || databaseUri, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+// cron.schedule('* * * * *', () => {
+
+// })
+scrape();
 
 app.listen(PORT, () => {
   log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
